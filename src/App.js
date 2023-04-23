fd49ea4ec3a46181db609de38f68d03ab8677fe7
axstutoring <kevintang01@ucla.edu>
2023-04-22 20:10:09 -0700

    Added aaron

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Tutoring from './Tutoring';
 import Home from './Home';
+import Aaron from './Aaron';
 import {
   BrowserRouter as Router,
   Routes,
@@ -11,13 +12,15 @@ function App() {
   return (
     <Router>
         <div id="navigation">
-        <Link to="/"></Link>
+          <Link to="/"></Link>
           <Link to="/tutoring"></Link>
+          <Link to="/aaron"></Link>
         </div>
         <div id="content">
           <Routes>
             <Route path="/" element={<Home />}/>
             <Route path="/tutoring" element={<Tutoring />}/>
+            <Route path="/aaron" element={<Aaron />}/>
           </Routes>
         </div>
     </Router>
