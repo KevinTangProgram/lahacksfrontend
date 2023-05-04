37b7bbba656e7e742a7b02b90ff72d4c4810bd13
axstutoring <kevintang01@ucla.edu>
2023-05-04 14:14:40 -0700

    Spaced textbox

--- src/Test.js
+++ src/Test.js
@@ -16,40 +16,45 @@ function Test()
             <img src="palm.png" id="homeImage" height="150" width="150" alt="Palm Tree"></img>
             <h1 className="centerVertically">Idea Oasis</h1>
         </div>
-        
-        <div className="tablet">
+
+        <div className="tablet" id="noBackground">
             <div className="threeButtons">
-                <button>Home</button>
-                <button>Your Oasis</button>
-                <button>About</button>
-            </div>
-            <div className="dateAndTime">
-                <p1 className="alignLeft">{new Date().toLocaleDateString()}</p1>
-                <p1 className="alignRight">{new Date().toLocaleTimeString()}</p1>
-            </div>
-            <h1>Title</h1>
-            <div className="twoButtons">
-                <button className="selectCells">Organized</button>
-                <button className="selectCells">Original Ideas</button>
-            </div>
-            <div>
-                <textarea placeholder="What stuff is your professor saying now?" value={shortInput} onChange={handleChangeInput}
-                // Add onKeyPress to add thought on enter
-                onKeyDown={(event) => { 
-                    if (event.key === "Enter" && event.shiftKey) 
-                    {
-                        event.preventDefault();
-                        setShortInput(shortInput+"\n")
-                    }
-                    else if (event.key === "Enter") 
-                    {
-                        const textarea = event.target;
-                        textarea.style.height = "auto";
-                        event.preventDefault(); 
-                    }}}
-                ></textarea>
+                <button className="selectCells">Home</button>
+                <button className="selectCells">Your Oasis</button>
+                <button className="selectCells">About</button>
             </div>
         </div>
+        <div className="backGround">
+            <div className="tablet">
+                <div className="dateAndTime">
+                    <p1 className="alignLeft">{new Date().toLocaleDateString()}</p1>
+                    <p1 className="alignRight">{new Date().toLocaleTimeString()}</p1>
+                </div>
+                <h1>Title</h1>
+                <div className="twoButtons">
+                    <button className="selectCells">Organized</button>
+                    <button className="selectCells">Original Ideas</button>
+                </div>
+                <div>
+                    <textarea placeholder="What stuff is your professor saying now?" value={shortInput} onChange={handleChangeInput}
+                    // Add onKeyPress to add thought on enter
+                    onKeyDown={(event) => { 
+                        if (event.key === "Enter" && event.shiftKey) 
+                        {
+                            event.preventDefault();
+                            setShortInput(shortInput+"\n")
+                        }
+                        else if (event.key === "Enter") 
+                        {
+                            const textarea = event.target;
+                            textarea.style.height = "auto";
+                            event.preventDefault(); 
+                        }}}
+                    ></textarea>
+                </div>
+            </div>
+        </div>
+        
         
         </>
     )
