e52e33a8fe6ea97fbaaf9048f09e5f41800beb53
aaronkwan <123356351+aaronkwan@users.noreply.github.com>
2023-05-10 00:27:51 -0700

    o/

--- src/components/generateUI.js
+++ src/components/generateUI.js
@@ -1,5 +1,9 @@
 import React, { useState } from 'react';
 import Tooltip from './tooltip';
+import { MessageProcessor } from '../utilities/messageProcesserFrontend';
+
+var options;
+
 
 function GenerationOptionsUI() {
     // UI Popups:
@@ -8,7 +12,8 @@ function GenerationOptionsUI() {
         setShowPopup(!showPopup);
     }
     // UI Sliders:
-    const [showCBMark, setShowCBMark] = useState(false);
+    const [showCheckBoxHeaders, setShowCheckBoxHeaders] = useState(false);
+    const [showCheckBoxBullet, setShowCheckBoxBullet] = useState(false);
     const [sliderValue, setSliderValue] = useState(0);
     const [generateDescElement, setGenerateDescElement] = useState(
         <div>
@@ -19,8 +24,9 @@ function GenerationOptionsUI() {
     const handleSliderChange = (event) => {
         const newValue = parseInt(event.target.value);
         setSliderValue(newValue);
-        if (newValue === 0) {
-            setShowCBMark(false);
+        if (newValue === 1) {
+            setShowCheckBoxHeaders(false);
+            setShowCheckBoxBullet(false);
             setGenerateDescElement(
                 <div>
                     <p>Format</p>
@@ -28,17 +34,9 @@ function GenerationOptionsUI() {
                 </div>
                 );
         }
-        if (newValue === 1) {
-            setShowCBMark(false);
-            setGenerateDescElement(
-                <div>
-                    <p>Organize</p>
-                    <Tooltip text={"Keep original wording."} />
-                </div>
-                );
-        }
         if (newValue === 2) {
-            setShowCBMark(true);
+            setShowCheckBoxHeaders(true);
+            setShowCheckBoxBullet(false);
             setGenerateDescElement(
                 <div>
                     <p>Polish</p>
@@ -47,7 +45,8 @@ function GenerationOptionsUI() {
             );
         }
         if (newValue === 3) {
-            setShowCBMark(true);
+            setShowCheckBoxHeaders(false);
+            setShowCheckBoxBullet(true);
             setGenerateDescElement(
                 <div>
                     <p>Elaborate</p>
@@ -59,7 +58,7 @@ function GenerationOptionsUI() {
     // UI Checkboxes:
     const [checkboxValues, setCheckboxValues] = useState({
         generateHeaders: false,
-        markUnclearMessages: false,
+        useBulletFormat: false,
     });
     const handleCheckboxChange = (event) => {
         setCheckboxValues({
@@ -69,14 +68,31 @@ function GenerationOptionsUI() {
     };
     // UI Warnings:
     const warningMessageContentTooLow = 
-        <div>Warning: Low content per message decreases the effectiveness of AI-enabled formatting.</div>
+        "Warning: Low content per message decreases the effectiveness of AI-enabled formatting."
     const warningMessageContentTooHigh =
         <div>Warning: Each message is counted as a single thought. Split large messages into different thoughts?</div>
     const warningNoteContentTooLow =
         <div>Warning: A lower number of messages decreases the effectiveness of AI-enabled formatting.</div>
     const warningNoteContentTooHigh =
         <div>Warning: Too many messages may result in usage limits being applied.</div>
+    const warningStrings = { warningMessageContentTooLow, warningMessageContentTooHigh, warningNoteContentTooLow, warningNoteContentTooHigh };
 
+    // UI Generation:
+    const [showWarnings, setShowWarnings] = useState(false);
+    const tryGeneration = (forceGenerate = false) => {
+        options = {
+            mode: sliderValue,
+            generateHeaders: checkboxValues.generateHeaders,
+            useBulletFormat: checkboxValues.useBulletFormat,
+        };
+        const warnings = MessageProcessor.getGenerationWarnings(options);
+        if (warnings.length < 0 || forceGenerate) {
+            MessageProcessor.startGenerationWithOptions(options);
+        }
+        else {
+            setShowWarnings(true);
+        }
+    }
 
     // Output:
     return (
@@ -99,7 +115,7 @@ function GenerationOptionsUI() {
                         {generateDescElement}
                         <input
                             type="range"
-                            min="0"
+                            min="1"
                             max="3"
                             step="1"
                             defaultValue={sliderValue}
@@ -107,6 +123,7 @@ function GenerationOptionsUI() {
                         />
                     </div>
                     {/* CHECKBOXES */}
+                    {showCheckBoxHeaders  && 
                     <div id="checkboxGenerateHeaders">
                         <p>Generate Headers:</p>
                         <input
@@ -115,28 +132,42 @@ function GenerationOptionsUI() {
                             checked={checkboxValues.generateHeaders}
                             onChange={handleCheckboxChange}
                         />
-                    </div>
-                    {showCBMark && 
-                    <div id="checkboxMarkUnclearMessages">
-                        <p>Mark Unclear Messages:</p>
+                    </div> 
+                    }
+                    {showCheckBoxBullet && 
+                        <div id="checkboxUseBulletFormat">
+                        <p>Use Bullet Format:</p>
                         <input
                             type="checkbox"
-                            name="markUnclearMessages"
-                            checked={checkboxValues.markUnclearMessages}
+                                name="useBulletFormat"
+                                checked={checkboxValues.useBulletFormat}
                             onChange={handleCheckboxChange}
                         />
                     </div>
                     }
                     {/* WARNINGS */}
-                    {(sliderValue === 2 || sliderValue === 3) && warningMessageContentTooLow}
-                    {(sliderValue === 2 || sliderValue === 3) && warningNoteContentTooLow}
-                    {warningMessageContentTooHigh}
-                    {warningNoteContentTooHigh}
-                    <button onClick={togglePopup}>GENERATE NOW</button>
+                    {showWarnings && 
+                        <div id="generateUIWarnings">
+                            <ul>
+                                {MessageProcessor.queueGenerationWithOptions(options).map((warning, index) => (
+                                    warning === "o_low" ? <li key={index}>{warningStrings.warningNoteContentTooLow}</li> :
+                                    warning === "o_high" ? <li key={index}>{warningStrings.warningNoteContentTooHigh}</li> :
+                                    warning === "m_low" ? <li key={index}>{warningStrings.warningMessageContentTooLow}</li> :
+                                    warning === "m_high" ? <li key={index}>{warningStrings.warningMessageContentTooHigh}</li> : null
+                                ))}
+                            </ul>
+                            <button onClick={() => tryGeneration(true)}>CONTINUE</button>
+                        </div>
+                    }
+                    {/* GENERATE BUTTON */}
+                    <button onClick={() => tryGeneration(false)}>GENERATE NOW</button>
                 </div>
             }
         </div>
     );
 }
+
+
+
 
 export default GenerationOptionsUI;
--- src/utilities/messageProcesserFrontend.js
+++ src/utilities/messageProcesserFrontend.js
@@ -6,6 +6,11 @@ export class MessageProcessor {
     static WARNING_MAX_MESSAGE_LENGTH = 250; // 1000 characters, ~50 words.
     static WARNING_MIN_MESSAGE_LENGTH = 20; // 20 characters, ~3-4 words.
     static WARNING_MIN_OASES_LENGTH = 4; // 4 messages.
+    static WARNING_MAX_OASES_LENGTH = 40; // 40 messages.
+    // Sync:
+    static syncedFromDatabase = false;
+    static guestUser = false;
+    static sessionIndex = 0; // Obtained from database, the true index of allRawMessages[0].
     // Raw Messages:
     static allRawMessages = []; // Array of strings, index corresponds to order.
     static lowContentMessageIndexes = []; // Array of indexes.
@@ -14,6 +19,9 @@ export class MessageProcessor {
     static allOrganizedMessages = []; // Array of objects {string, ID}, index corresponds to order.
     static headerMessageIndexes = []; // Array of indexes.
     static markedGPTMessageIndexes = [];
+    // Generation:
+    static isGenerating = false;
+    static currentIndex = 0; // Index of the message currently being generated.
 
     // Functionality:
     // 1. Add, Remove, and Edit Messages.
@@ -23,10 +31,129 @@ export class MessageProcessor {
     // 5. Connect with backend for GPT-3 generation.
     // 6. Handle API failures.
 
+    // 1:
     static addMessage(newMessage) {
+        if (!this.readyForMessages()) {
+            return false;
+        }
         this.allRawMessages.push(newMessage);
+        this.syncToDatabase;
+        // Grab index, check for warnings, return:
+        const index = this.allRawMessages.indexOf(newMessage) + this.sessionIndex;
+        this.handleMessageWarnings(index, newMessage);
+        return index;
+    }
+    static removeMessage(masterIndex) {
+        if (masterIndex < this.sessionIndex) {
+            // Deleting a message from previous session:
+            editMessageInDatabase(masterIndex, "");
+            return;
+        }
+        // Delete a message from this session:
+        this.allRawMessages.splice(masterIndex - this.sessionIndex, 1);
+        handleMessageWarnings(masterIndex - this.sessionIndex, "");
+            // remember to update the component ID of messages following the splice.
+        this.syncToDatabase;
+    }
+    static editMessage(masterIndex, messageString) {
+        if (masterIndex < this.sessionIndex) {
+            // Editing a message from previous session:
+            editMessageInDatabase(masterIndex, messageString);
+            return;
+        }
+        // Editing a message from this session:
+        this.allRawMessages[masterIndex - this.sessionIndex] = messageString;
+
+    }
+    
+    // 2:
+    static trackMessageProperties() {
+
+    }
+    static trackOasisProperties() {
+
     }
 
+    // 3:
+    handleMessageWarnings(index, newMessage) {
+        if (newMessage === "") {
+            // Deleting message:
+            removeMessageWarnings(index);
+            return;
+        }
+        // Edit or adding message:
+        if (newMessage.size > this.WARNING_MAX_MESSAGE_LENGTH) {
+            highContentMessageIndexes.push(index + sessionIndex);
+            return "max_length";
+        }
+        if (newMessage.size < this.WARNING_MIN_MESSAGE_LENGTH) {
+            lowContentMessageIndexes.push(index + sessionIndex);
+            return "min_length";
+        }
+        this.removeMessageWarnings(index);
+    }
+    removeMessageWarnings(index) {
+        if (highContentMessageIndexes.includes(index)) {
+            highContentMessageIndexes.splice(highContentMessageIndexes.indexOf(index), 1);
+        }
+        if (lowContentMessageIndexes.includes(index)) {
+            lowContentMessageIndexes.splice(lowContentMessageIndexes.indexOf(index), 1);
+        }
+    }
 
+    // 4:
+    static syncFromDatabase() {
+        // Get sessionIndex from database:
+        // Unpack most recent 'payload' into allRawMessages:
+        // Record how many payloads are in database:
+        // Set bool to true:
+        this.syncedFromDatabase = true;
+    }
+    static loadPayLoadFromDatabase(payloadIndex) {
+        // Load specific payload from database:
+    }
+    static readyForMessages() {
+        return this.syncedFromDatabase || this.guestUser;
+    }
+    static editMessageInDatabase(index, messageString) {
+        // Set message in database to string:
+    }
+    static syncToDatabase() {
+        // Updates 'payload' with all messages from this session:
+        // If 'payload' is over 50, start new payload:
+    }
 
+    // 5:
+    static getGenerationWarnings(options) {
+        // Give warnings:
+        let warnings = [];
+        if (this.lowContentMessageIndexes.length > 0) {
+            warnings.push("m_low");
+        }
+        if (this.highContentMessageIndexes.length > 0) {
+            warnings.push("m_high");
+        }
+        if (this.allRawMessages.length < this.WARNING_MIN_OASES_LENGTH) {
+            warnings.push("o_low");
+        }
+        if (this.allRawMessages.length > this.WARNING_MAX_OASES_LENGTH) {
+            warnings.push("o_high");
+        }
+        return warnings;
+    }
+    static startGenerationWithOptions(options) {
+        if (this.isGenerating) {
+            return false;
+        }
+        this.isGenerating = true;
+        // Process messages, send to backend:
+        console.log("Processing messages with mode X" + options.mode);
+
+    }
+    static queueGeneration() {
+        // Queue generation request:
+    }
+    static processGenerationResponse(response) {
+        // Clean and process messages, add to allOrganizedMessages:
+    }
 }
