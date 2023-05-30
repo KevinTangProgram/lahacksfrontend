7637e730a71185aa9059c8e0d20a9bbf2596137c
axstutoring <kevintang01@ucla.edu>
2023-05-29 21:25:24 -0700

    Trying to center

--- src/Login.js
+++ src/Login.js
@@ -3,31 +3,11 @@ import './CSS/Login.css';
 import React, { useState, useEffect } from 'react';
 import CreateAccountUI from './components/createAccountUI';
 import LoginAccountUI from './components/loginAccountUI';
-import { GoogleLogin } from 'react-google-login';
-import { gapi } from 'gapi-script';
-//
-
-const client_id = "880561077463-92vhkg43l0fp5v93fdkgkkauf6dbok5s.apps.googleusercontent.com";
 
 function Login()
 {
     // Create Acc:
     const [showCreateAcc, setShowCreateAcc] = useState(false);
-
-    useEffect(() => {
-        function start() {
-            gapi.client.init({
-                clientId: client_id,
-                scope: ""        
-            })
-        };
-        gapi.load('client:auth2', start);
-        // function onSignIn(googleUser) {
-        //     var profile = googleUser.getBasicProfile();
-        //     console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-        //     console.log('Name: ' + profile.getName());
-        // }
-    }, []);
 
     return (<>
         <button id="backButton" className="notWhite" onClick={() => {
@@ -38,22 +18,6 @@ function Login()
         {!showCreateAcc && <LoginAccountUI setShowCreateAcc={setShowCreateAcc} />}
         {/* CREATE ACC */}
         {showCreateAcc && <CreateAccountUI setShowCreateAcc={setShowCreateAcc} />}
-
-        <div>
-            <GoogleLogin
-                clientId={client_id}
-                buttonText="Sign in with Google"
-                onSuccess={(res) => {
-                    window.location.href = "/";
-                    //console.log(res);             //does not work because of widows.location.href. safe the information to a local variable
-
-                }}
-                onFailure={(res) => {
-                    console.log(res);
-                }}
-                cookiePolicy={'single_host_origin'}
-            />
-        </div>
     </>
     )
 }
--- src/components/loginAccountUI.js
+++ src/components/loginAccountUI.js
@@ -1,7 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import {CONST} from '../utilities/CONST.js';
+import '../CSS/Login.css';
+import { GoogleLogin } from 'react-google-login';
+import { gapi } from 'gapi-script';
 //
+
+const client_id = "880561077463-92vhkg43l0fp5v93fdkgkkauf6dbok5s.apps.googleusercontent.com";
 
 function LoginAccountUI(props) {
     // Textboxes:
@@ -9,6 +14,22 @@ function LoginAccountUI(props) {
     const [password, setPassword] = useState("");
     const handleChangeUsername = (event) => { setUsername(event.target.value); }
     const handleChangePassword = (event) => { setPassword(event.target.value); }
+
+    useEffect(() => {
+        function start() {
+            gapi.client.init({
+                clientId: client_id,
+                scope: ""        
+            })
+        };
+        gapi.load('client:auth2', start);
+        // function onSignIn(googleUser) {
+        //     var profile = googleUser.getBasicProfile();
+        //     console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+        //     console.log('Name: ' + profile.getName());
+        // }
+    }, []);
+
     // Login:
     function loginAccount() {
         axios.get(CONST.URL + "/login", {
@@ -34,21 +55,36 @@ function LoginAccountUI(props) {
     // Output:
     return (
         <>
-            <h1 style={{"text-align": "center"}}>Login</h1>
+            <h1 style={{"text-align": "center"}}>Welcome Back!</h1>
             <div className="selectGridSmall">
-                <input placeholder="Username" value={username} onChange={handleChangeUsername}></input>
-                <br></br>
-                <input placeholder="Password" type="password" value={password} onChange={handleChangePassword} ></input>
-                <br></br>
-                <button className="selectCells" id="submitAndConfirmLong" style={{"border-radius": "1em", "height": "2em", "width": "80%"}} onClick={() => {
+                <input placeholder="Email address" value={username} onChange={handleChangeUsername}></input>
+                <input placeholder="Password" type="password" value={password} onChange={handleChangePassword} style={{"margin-top": "20px"}}></input>
+                <div style={{"font-size": "12px", "margin-top": "10px"}}>
+                    <p1>Don't have an account? </p1>
+                    <button style={{"text-decoration": "none", "border": "none", "background-color": "transparent", "cursor": "pointer", "color": "#10a37f"}} onClick={() => {
+                    props.setShowCreateAcc(true);}}>Sign up</button>
+                </div>
+                <button className="selectCells" id="submitAndConfirmLong" style={{"border-radius": "1em", "height": "2em", "width": "80%", "margin-top": "20px"}} onClick={() => {
                     loginAccount();
                 }}>Login</button>
-                <br></br>
-                <p1>or</p1>
-                <br></br>
-                <button className="selectCells" id="submitAndConfirmLong" style={{"border-radius": "1em", "height": "2em", "width": "80%"}} onClick={() => {
-                    props.setShowCreateAcc(true);
-                }}>Create Account</button>
+                <div style={{"margin-top": "20px"}}>
+                    <p1>or</p1>
+                </div>
+            </div>
+            <div style={{"display": "flex", "align-items": "center"}}>
+                <GoogleLogin
+                    clientId={client_id}
+                    buttonText="Sign in with Google"
+                    onSuccess={(res) => {
+                        //window.location.href = "/";
+                        console.log(res);             //does not work because of widows.location.href. save the information to a local variable
+
+                    }}
+                    onFailure={(res) => {
+                        console.log(res);
+                    }}
+                    cookiePolicy={'single_host_origin'}
+                />
             </div>
         </>
         
