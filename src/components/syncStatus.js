3ed88769f41e5768ecf76ed8b0031e294589184c
aaronkwan <123356351+aaronkwan@users.noreply.github.com>
2023-08-21 12:37:25 -0700

    o/

---
+++ src/components/syncStatus.js
@@ -0,0 +1,23 @@
+//
+import { StorageManager } from '../utilities/storageManager';
+import Observer from '../components/observer';
+import '../CSS/Utils.css';
+
+function SyncStatus() {
+    // Display the current status of StorageManager.state:
+    return (
+        <Observer dependencies={"StorageState"} Component={() => {
+            return (
+               <div className="icon-container">
+                    {StorageManager.unsyncCounter === 0 ? (
+                        <img src="/images/icons/iconConfirm.png" alt="Synced" />
+                    ) : (
+                        <div className="loader" alt="Loading"></div>
+                    )}
+                </div> 
+            );
+        }} />
+    );
+}
+
+export default SyncStatus;
--- src/tabs/Tab_settings.js
+++ src/tabs/Tab_settings.js
@@ -1,11 +1,9 @@
-import '../CSS/Test.css';
 import React, { useState } from 'react';
 import Tab_settings_user from './Tab_settings_user';
 import Tab_settings_oasis from './Tab_settings_oasis';
 import Clock from '../components/clock';
-import Observer from '../components/observer';
 import '../CSS/Tab_oasis.css';
-import { StorageManager } from '../utilities/storageManager';
+import SyncStatus from '../components/syncStatus';
 //
 function Tab_settings(props) {
     // Tab Navigation:
@@ -17,14 +15,7 @@ function Tab_settings(props) {
                 {/* start banner */}
                 <div className="dateAndTime">
                     <Clock type={"date"} className={"alignLeft"} />
-                    <Observer dependencies={"StorageState"} Component={() => {
-                        if (StorageManager.unsyncCounter === 0) {
-                            // Synced:
-                            return <img className="iconSync" src="/images/icons/iconConfirm.png" alt="Synced" />
-                        }
-                        // Syncing:
-                        return <div className="loader iconSync"></div> 
-                        }} />
+                    <SyncStatus />
                     <h3 className="alignCenter">Settings</h3>
                     <Clock type={"time"} className={"alignRight"} />
                 </div>
